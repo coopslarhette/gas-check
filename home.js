f234810e1b4e9fe9e3f1151703008eff4f02559b
coopslarhette <coopslarhette@gmail.com>
2019-07-02 21:03:34 -0700

    adds working distanceMatrix code YEET

--- home.js
+++ home.js
@@ -3,9 +3,38 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {
-      lat: 33.969873,
-      lng: -118.416671
+      lat: 39.956813,
+      lng: -102.011721
     },
-    zoom: 9
+    zoom: 3
   });
+}
+
+
+function computeCost() {
+  var origin = document.getElementById('origin').value;
+  var destination = document.getElementById('destination').value;
+  var service = new google.maps.DistanceMatrixService();
+  service.getDistanceMatrix({
+    origins: [origin],
+    destinations: [destination],
+    travelMode: 'DRIVING',
+    unitSystem: google.maps.UnitSystem.IMPERIAL,
+    avoidHighways: false,
+    avoidTolls: false,
+  }, callback);
+}
+
+function callback(response, status) {
+  if (status == 'OK') {
+    var origins = response.originAddresses;
+    var destinations = response.destinationAddresses;
+    var results = response.rows[0].elements;
+    var element = results[0];
+    var distance = element.distance.text;
+    var duration = element.duration.text;
+    alert("dist: " + distance + ", duration: " + duration);
+  } else {
+    alert("error message: " + status);
+  }
 }
