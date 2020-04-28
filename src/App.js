0a730ff0b658e41633494461aa4eddeea5912d59
coopslarhette <coopslarhette@gmail.com>
2020-04-27 18:41:17 -0700

    init commit and navbar done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import GaugeNavBar from './GaugeNavBar'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Container from 'react-bootstrap/Container'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <GaugeNavBar/>
+      </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
