2dbcfebd295f0f9fd71efb2a584ed394243b9e15
coopslarhette <coopslarhette@gmail.com>
2020-07-11 12:59:02 -0700

    adds jest to eslint env

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
+    jest: true
   },
   extends: [
     'plugin:react/recommended',
@@ -23,7 +24,7 @@ module.exports = {
     'react',
   ],
   rules: {
-    semi: [2, "never"],
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    semi: [2, 'never'],
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
   }
-};
+}
