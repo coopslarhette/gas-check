db9142f141409f911766d55cad9dd60c4906c157
coopslarhette <coopslarhette@gmail.com>
2020-07-19 14:49:40 -0700

    adds a bunch of Google Maps API stuff, changes component to class

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,27 +1,76 @@
 import React from 'react'
-import GoogleMapReact from 'google-map-react'
 import './Map.css'
 
-function Map(): JSX.Element {
-  const center = {
-    lat: 39.956813,
-    lng: -102.011721,
+// eslint-disable-next-line react/prefer-stateless-function
+class Map extends React.Component {
+  componentDidMount(): void {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+    // @ts-ignore
+    if (!window.google) {
+      const s = document.createElement('script')
+      s.type = 'text/javascript'
+      s.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyC1rUOvjD8PT8XlKlL6uXXaq6wl_9lIOWg'
+      const x = document.getElementsByTagName('script')[0]
+      if (x.parentNode) {
+        x.parentNode.insertBefore(s, x)
+      }
+      // Below is important.
+      // We cannot access google.maps until it's finished loading
+      s.addEventListener('load', (e) => {
+        this.doInitMapLogic()
+      })
+    } else {
+      this.doInitMapLogic()
+    }
   }
 
-  // const geolocation = useGeolocation()
-  // useEffect(() => [center.lat, center.lng] = [geolocation.latitude, geolocation.longitude])
-  return (
-    <div className="map">
-      <GoogleMapReact
-        /* eslint-disable-next-line @typescript-eslint/ban-ts-ignore */
-        // @ts-ignore
-        bootstrapURLKeys="AIzaSyC1rUOvjD8PT8XlKlL6uXXaq6wl_9lIOWg"
-        zoom={3}
-        center={center}
-        yesIWantToUseGoogleMapApiInternals
-      />
-    </div>
-  )
+  doInitMapLogic(): void {
+    // eslint-disable-next-line no-undef,@typescript-eslint/ban-ts-ignore
+    // @ts-ignore
+    const directionsDisplay = new google.maps.DirectionsRenderer()
+    // need this for autocomplete to work on origin input
+    // const autocompleteOrigin = new google.maps.places.Autocomplete(document.getElementById('origin'ip)
+    // const autocompleteDest = new google.maps.places.Autocomplete(document.getElementById('destination'))
+    // const directionsService = new google.maps.DirectionsService()
+    // const directionsService = new google.maps.DirectionsService()
+    // eslint-disable-next-line no-undef,@typescript-eslint/ban-ts-ignore
+    // @ts-ignore
+    const map = new google.maps.Map(document.getElementsByClassName('map')[0], {
+      center: {
+        lat: 39.956813,
+        lng: -102.011721,
+      },
+      zoom: 3,
+    })
+    directionsDisplay.setMap(map)
+  }
+
+  render(): JSX.Element {
+    // useEffect(() => {
+    //   const googleMapsUrl = 'https://maps.google.com/maps/api/js?key=AIzaSyC1rUOvjD8PT8XlKlL6uXXaq6wl_9lIOWg'
+    //
+    //   if (!document.querySelectorAll(`[src="${googleMapsUrl}"]`).length) {
+    //     document.body.appendChild(Object.assign(
+    //       document.createElement('script'), {
+    //         type: 'text/javascript',
+    //         src: googleMapsUrl,
+    //         onload: () => doInitMapLogic(),
+    //       },
+    //     ))
+    //     const s = document.createElement('script')
+    //     const x = document.getElementsByTagName('script')[0]
+    //     if (x.parentNode != null) {
+    //       x.parentNode.insertBefore(s, x)
+    //     }
+    //   } else {
+    //     doInitMapLogic()
+    //   }
+    // })
+
+    return (
+      <div className="map" />
+    )
+  }
 }
 
 export default Map
