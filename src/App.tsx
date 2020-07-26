75ed57d3a1c1d95452d3a997214708e6491a1f83
coopslarhette <coopslarhette@gmail.com>
2020-07-26 14:07:45 -0700

    removes InputOutputArea.tsx and adds beginning of maps request logic

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,32 @@ import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import GaugeNavBar from './GaugeNavBar'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import InputOutputArea from './InputOutputArea'
 import Map from './Map'
+import InputArea from './InputArea'
 
 function App(): JSX.Element {
+  let request: { origin: string; destination: string; travelMode: string } | null = null
+
+  const startMapRequest = (origin: string, destination: string): void => {
+    request = {
+      origin,
+      destination,
+      travelMode: 'DRIVING',
+    }
+  }
   return (
     <div className="App">
       <Container>
         <GaugeNavBar />
         {/* ideally we want this to wrap the map to the bottom aat 768px width as that's the
-        smallest width the input boxes still render nicely at will fix later probably */}
+        smallest width the input boxes still render nicely at TODO will fix later probably */}
         <Row className="interactionSpace">
           <Col sm={7}>
-            <InputOutputArea />
+            <InputArea handleClick={startMapRequest} />
+            {/*  output area */}
           </Col>
           <Col sm={5}>
-            <Map />
+            <Map request={request} />
           </Col>
         </Row>
       </Container>
