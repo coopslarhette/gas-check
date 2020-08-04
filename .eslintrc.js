4c243c05f6cf180bb452c7ccf540b58f45e16d28
coopslarhette <coopslarhette@gmail.com>
2020-08-03 22:01:40 -0700

    cleans and reinstalls a many dependencies related to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,17 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    jest: true,
-  },
-  extends: [
-    'airbnb',
-    'plugin:react/recommended',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
+  root: true,
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
   plugins: [
-    'react',
     '@typescript-eslint',
+  ],
+  extends: [
+    'airbnb-typescript',
   ],
   rules: {
     semi: [2, 'never'],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        mjs: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-    'react/jsx-filename-extension': [2, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
+    "@typescript-eslint/semi": [2, 'never'],
   },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        moduleDirectory: ['node_modules', 'src/'],
-      },
-    },
+  parserOptions: {
+    project: './tsconfig.json',
   },
-}
+};
