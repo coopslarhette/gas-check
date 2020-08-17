44bacbf2f8f1fbe0611f032e3906bcd39cb885b5
coopslarhette <coopslarhette@gmail.com>
2020-08-16 18:38:34 -0700

    refactors componentDidMount, extracts variables in drawPath and refactors getCostAndTime into an arrow fn

--- src/Map.tsx
+++ src/Map.tsx
@@ -13,6 +13,15 @@ class Map extends Component<MyProps> {
   directionsService
 
   componentDidMount(): void {
+    this.attachMapsScript()
+  }
+
+  componentDidUpdate(): void {
+    this.drawPath()
+    this.callDistanceMatrix()
+  }
+
+  attachMapsScript(): void {
     if (!window.google) {
       const s = document.createElement('script')
       s.type = 'text/javascript'
@@ -31,47 +40,52 @@ class Map extends Component<MyProps> {
     }
   }
 
-  componentDidUpdate(): void {
-    this.drawPath()
-    this.callDistanceMatrix()
-  }
-
-  getCostAndTime(response: google.maps.DistanceMatrixResponse, status: string): void {
-    const { handleComputeResult } = this.props
-    if (status === 'OK') {
-      const result = response.rows[0].elements[0]
-      const distance = result.distance.value
-      const duration = result.duration.text
-      console.table([distance, duration])
-      handleComputeResult(distance, duration)
-    }
-  }
-
   drawPath(): void {
     const { origin, destination } = this.props
-    this.directionsService.route({ origin, destination, travelMode: 'DRIVING' }, (response, status) => {
-      if (status === 'OK') {
-        this.directionsDisplay.setDirections(response)
-      } else if (status === 'NOT_FOUND' || status === 'ZERO_RESULTS') {
-        // eslint-disable-next-line no-alert
-        alert('One of your addresses could not be found, please try again.')
-      } else {
-        // eslint-disable-next-line no-alert
-        alert('Something went wrong, please try again later.')
-      } // need to figure out better handling of this methinks
-    })
+    const request = {
+      origin,
+      destination,
+      travelMode: 'DRIVING',
+    }
+
+    // infinite loop here
+    this.directionsService.route(
+      request, (response: google.maps.DirectionsResult, status: string) => {
+        if (status === 'OK') {
+          this.directionsDisplay.setDirections(response)
+        } else if (status === 'NOT_FOUND' || status === 'ZERO_RESULTS') {
+          // eslint-disable-next-line no-alert
+          alert('One of your addresses could not be found, please try again.')
+        } else {
+          // eslint-disable-next-line no-alert
+          alert('Something went wrong, please try again later.')
+        } // need to figure out better handling of this methinks
+      },
+    )
   }
 
   callDistanceMatrix(): void {
     const { origin, destination } = this.props
     const service = new google.maps.DistanceMatrixService()
-    service.getDistanceMatrix({
+    const request: google.maps.DistanceMatrixRequest = {
       origins: [origin],
       destinations: [destination],
       // @ts-ignore
       travelMode: 'DRIVING',
       unitSystem: google.maps.UnitSystem.IMPERIAL,
-    }, this.getCostAndTime)
+    }
+
+    service.getDistanceMatrix(
+      request, (response: google.maps.DistanceMatrixResponse, status: string): void => {
+        const { handleComputeResult } = this.props
+        if (status === 'OK') {
+          const result = response.rows[0].elements[0]
+          const distance = result.distance.value
+          const duration = result.duration.text
+          handleComputeResult(distance, duration)
+        }
+      },
+    )
   }
 
   doInitMapLogic(): void {
