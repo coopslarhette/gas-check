940785ac4533ae5cb2e7cadaf38e4fb8e634031f
coopslarhette <coopslarhette@gmail.com>
2020-08-24 16:28:04 -0700

    Updates

---
+++ precache-manifest.0f911a35e193dbaa2267fa297338e188.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8d69667690ce15e81726e586d4f577",
+    "url": "/gauge/index.html"
+  },
+  {
+    "revision": "2bf143dde6fffe82e8f2",
+    "url": "/gauge/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "007d46c0e3b88a42727e",
+    "url": "/gauge/static/css/main.17c920d7.chunk.css"
+  },
+  {
+    "revision": "2bf143dde6fffe82e8f2",
+    "url": "/gauge/static/js/2.0e9da325.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gauge/static/js/2.0e9da325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "007d46c0e3b88a42727e",
+    "url": "/gauge/static/js/main.f79f0285.chunk.js"
+  },
+  {
+    "revision": "c621ea0f738567194de0",
+    "url": "/gauge/static/js/runtime-main.3f8a947d.js"
+  }
+]);
